02c6d30f5efcf9409f65620934598b8e2d3aa849
Карась Анатолий <karas@technocom.tech>
2021-11-19 11:07:39 +0500

    feat: when updating a playlist, check whether a track belongs to your playlist

--- src/main/java/com/github/aldebaranoro/apollonmusicresourceserver/api/controller/PlaylistController.java
+++ src/main/java/com/github/aldebaranoro/apollonmusicresourceserver/api/controller/PlaylistController.java
@@ -1,6 +1,7 @@
 package com.github.aldebaranoro.apollonmusicresourceserver.api.controller;
 
 import com.github.aldebaranoro.apollonmusicresourceserver.api.model.entity.Playlist;
+import com.github.aldebaranoro.apollonmusicresourceserver.api.model.entity.Track;
 import com.github.aldebaranoro.apollonmusicresourceserver.api.model.mapper.PlaylistMapper;
 import com.github.aldebaranoro.apollonmusicresourceserver.api.model.view.PlaylistCreate;
 import com.github.aldebaranoro.apollonmusicresourceserver.api.model.view.PlaylistRead;
@@ -17,6 +18,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/users/me/playlists")
@@ -75,6 +78,7 @@ class PlaylistController {
         playlist.setId(id);
         playlist.setDiscordIdentity(KeycloakPrincipalUtils.getIdentityProviderId(principal));
         playlist.setUserId(KeycloakPrincipalUtils.getUserId(principal));
+        checkPlaylistTrackIds(playlist);
         return mapper.toViewRead(
                 playlistRepository.save(playlist)
         );
@@ -87,5 +91,34 @@ class PlaylistController {
                 .orElseThrow(() -> new RuntimeException("Не найдена сущность с заданным Id!"));
         playlistRepository.deleteById(id);
         return playlist;
+    }
+
+    /**
+     * Проверка на наличие чужеродных треков
+     *
+     * @param updatedPlaylist плейлист с изменениями
+     */
+    private void checkPlaylistTrackIds(Playlist updatedPlaylist) {
+        Playlist playlist = playlistRepository.getById(updatedPlaylist.getId());
+        Set<Long> trackIds = getTrackIds(playlist);
+        Set<Long> updatedTrackIds = getTrackIds(updatedPlaylist);
+
+        updatedTrackIds.forEach(id -> {
+            if (id != null && !trackIds.contains(id)) {
+                String message = String.format(
+                        "Нельзя добавить в плейлист трек с id = %s, так как он принадлежит другому плейлисту!",
+                        id
+                );
+                throw new RuntimeException(message);
+            }
+        });
+    }
+
+    private Set<Long> getTrackIds(Playlist playlist) {
+        return playlist
+                .getTracks()
+                .stream()
+                .map(Track::getId)
+                .collect(Collectors.toSet());
     }
 }
