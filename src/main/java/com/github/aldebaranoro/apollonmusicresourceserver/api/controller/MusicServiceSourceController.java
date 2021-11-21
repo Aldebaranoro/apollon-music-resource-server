a7e914d27fa77647576e8d543a760fb4087c4bdf
Aldebaranoro <anatol.karas@gmail.com>
2021-11-21 14:30:49 +0500

    feat: music services now public

--- src/main/java/com/github/aldebaranoro/apollonmusicresourceserver/api/controller/MusicServiceSourceController.java
+++ src/main/java/com/github/aldebaranoro/apollonmusicresourceserver/api/controller/MusicServiceSourceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/music-services")
+@RequestMapping("public/api/v1/music-services")
 @RequiredArgsConstructor
 public class MusicServiceSourceController {
 
